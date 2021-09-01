36fa97e77b0775c8dc0f99b30028f57ef94716d5
Pablo de Haro <pablitar@gmail.com>
2021-09-01 10:12:29 -0300

    Harcoded request to consistency with Jmeter test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { sleep } from "k6"
+import { sleep } from "k6";
 import http from "k6/http"
 
 import { sample } from "./sample.js"
@@ -8,8 +8,8 @@ const getSamples = sample.filter(it => it.includes("GET"))
 const catalystUrl = "https://peer-ue-2.decentraland.zone"
 
 export let options = {
-  vus: 10,
-  duration: '30s',
+  vus: 200,
+  duration: '600s',
 };
 
 function randomSample() {
@@ -18,9 +18,9 @@ function randomSample() {
 
 export default function() {
   const request = randomSample()
-  const url = catalystUrl + request.replace(/\w+ (.*) HTTP.*/, "$1")
+  // const url = catalystUrl + request.replace(/\w+ (.*) HTTP.*/, "$1")
+  const url = catalystUrl + "/content/entities/scene?pointer=0,0&pointer=0,1&pointer=0,2&pointer=0,3&pointer=0,4&pointer=0,5&pointer=0,6&pointer=1,0&pointer=1,1&pointer=1,2&pointer=1,3&pointer=1,4&pointer=1,5&pointer=1,6&pointer=2,0&pointer=2,1&pointer=2,2&pointer=2,3&pointer=2,4&pointer=2,5&pointer=2,6&pointer=3,0&pointer=3,1&pointer=3,2&pointer=3,3&pointer=3,4&pointer=3,5&pointer=3,6&pointer=4,0&pointer=4,1&pointer=4,2&pointer=4,3&pointer=4,4&pointer=4,5&pointer=4,6&pointer=5,0&pointer=5,1&pointer=5,2&pointer=5,3&pointer=5,4&pointer=5,5&pointer=5,6&pointer=6,0&pointer=6,1&pointer=6,2&pointer=6,3&pointer=6,4&pointer=6,5&pointer=6,6"
 
   http.get(url)
-
-  sleep(1)
+  sleep(0.5)
 }
